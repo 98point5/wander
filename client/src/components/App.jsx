378470d1ff365d5f1a2e73efbbf18106172f75a3
Michael Chan <mikey.chan1@gmail.com>
2018-09-22 16:57:51 -0700

    Search component

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -2,41 +2,63 @@ import React from 'react';
 import axios from 'axios';
 import Start from './Start.jsx';
 import Search from './Search.jsx';
-import Map from './Map.jsx';
+// import Map from './Map.jsx';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      page: 'start'
+      page: 'start',
+      destination: '',
     };
-    this.changePageToSearch = this.changePageToSearch.bind();
+    this.handleWander = this.handleWander.bind(this);
+    this.handleSearch = this.handleSearch.bind(this);
+    this.inputSearch = this.inputSearch.bind(this);
   }
 
-  changePageToSearch() {
+  handleWander() {
+    //call specifc route to unlock
+    //change page state
+
     this.setState({
-      page: 'search'
+      page: 'search',
     });
+    console.log(this.state);
+  }
+
+  handleSearch() {
+    this.setState({
+      page: 'map',
+    });
+    console.log('MAP', this.state);
+  }
+
+  inputSearch(e) {
+    this.setState({
+      destination: e.target.value,
+    });
+    console.log(this.state);
   }
 
   render() {
     if (this.state.page === 'start') {
-      return (
-          <Start changePage={this.changePageToSearch}/> 
-      );
+      return <Start changePage={this.handleWander} />;
     }
 
     if (this.state.page === 'search') {
       return (
-          <Search /> 
+        <Search
+          handleSearch={this.handleSearch}
+          inputSearch={this.inputSearch}
+        />
       );
     }
 
-    if (this.state.page === 'map') {
-      return (  
-          <Map />
-      );
-    }
+    // if (this.state.page === 'map') {
+    //   return (
+    // <Map />
+    //   );
+    // }
   }
 }
 
---
+++ client/src/components/Search.jsx
@@ -0,0 +1,86 @@
+import React, { Component } from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import classNames from 'classnames';
+import { TextField } from '@material-ui/core';
+import purple from '@material-ui/core/colors/purple';
+import Button from '@material-ui/core/Button';
+
+const styles = theme => ({
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    color: 'white',
+  },
+  margin: {
+    margin: theme.spacing.unit,
+  },
+  cssLabel: {
+    '&$cssFocused': {
+      color: purple[500],
+    },
+  },
+  cssFocused: {},
+  cssUnderline: {
+    '&:after': {
+      borderBottomColor: purple[500],
+    },
+  },
+  cssRoot: {
+    color: theme.palette.getContrastText(purple[500]),
+    backgroundColor: purple[500],
+    opacity: 0.2,
+    '&:hover': {
+      backgroundColor: purple[700],
+    },
+  },
+});
+
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      search: '',
+    };
+  }
+
+  search() {
+    //do API call to google maps to typed in search address
+    this.setState({});
+  }
+
+  render() {
+    const { classes } = this.props;
+    return (
+      <div>
+        <form className={classes.container} noValidate autoComplete="off">
+          <TextField
+            autoFocus={true}
+            type="text"
+            placeholder="Wander to..."
+            // value={this.props.value}
+            onKeyUp={this.props.inputSearch}
+            id="full-width"
+            InputLabelProps={{ shrink: true }}
+            fullWidth
+            margin="normal"
+          />
+          <FormHelperText id="name-helper-text" />
+        </form>
+
+        <div className={classes.container}>
+          <Button
+            variant="contained"
+            color="primary"
+            className={classNames(classes.margin, classes.cssRoot)}
+            onClick={this.props.handleSearch}
+          >
+            Search
+          </Button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default withStyles(styles)(SearchBar);

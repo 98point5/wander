53b52fb5bcf774e026865d5621884e03cb557022
Kony Pham <coniekp@icloud.com>
2018-09-22 14:48:43 -0700

    set up server index

---
+++ server/index.js
@@ -0,0 +1,17 @@
+
+const express = require('express');
+const app = express();
+
+app.use(express.static('dist'));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.post('/wander/connect', ()=>console.log('handler'));
+app.get('/wander/unlock', ()=>console.log('handler')));
+app.get('/wander/lock',()=>console.log('handler') );
+app.get('/wander/location', ()=>console.log('handler'))
+app.get('/wander/routes/:destinationId',()=>console.log('handler') )
+app.get('/wander/routes/new', ()=>console.log('handler'));
+
+app.listen(3000, () => console.log('Listening at 3000'));
+

de7eede6796b02bc4ac9e19f1c5bd42ad2285ea3
Michael Chan <mikey.chan1@gmail.com>
2018-09-22 22:36:24 -0700

    align button css

--- client/src/components/Start.jsx
+++ client/src/components/Start.jsx
@@ -27,7 +27,7 @@ const styles = theme => ({
     },
   },
   cssRoot: {
-    background: 'linear-gradient(45deg, #FE6B8B 70%, #FF8E53 90%)',
+    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
     borderRadius: 3,
     border: 0,
     color: 'white',
@@ -35,7 +35,7 @@ const styles = theme => ({
     padding: '0 30px',
     boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
     '&:hover': {
-      background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 100%)',
+      background: 'linear-gradient(45deg, #FE6B8B 70%, #FF8E53 100%)',
     },
   },
 });
